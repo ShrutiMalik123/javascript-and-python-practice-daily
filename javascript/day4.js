7213d39efafa659a0df13959e06f1aa0bb5821a2
Shruti Malik <shruti.malik.ramaswamy@gmail.com>
2020-06-23 13:05:31 -0500

    day4.js

--- javascript/day4.js
+++ javascript/day4.js
@@ -12,3 +12,13 @@
 
 //Input: 120
 //Output: 21
+var reverse = function (x) {
+  // Turn input into String, then reverse
+  let rev = x.toString().split("").reverse().join("");
+
+  // Turn back to number, add sign
+  rev = parseInt(rev) * Math.sign(x);
+
+  // check if result in the range
+  return rev > 2 ** 31 - 1 || rev < -(2 ** 31) ? 0 : rev;
+};
